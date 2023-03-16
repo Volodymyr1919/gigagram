a1a9a83235028c9f3005797788733d0a71d33c03
Volodymyr1919 <108761262+Volodymyr1919@users.noreply.github.com>
2023-03-16 21:28:41 +0100

    adding outlet, signin and not found pages

---
+++ src/pages/public/landing/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import footerm from "./footerm.scss";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Footer</p>
+        </footer>
+    );
+};
