14855164c2824a1aee8f0bd9b4a1ecebd8ce2d52
AlexElya <aleksnosov86@gmail.com>
2023-03-20 14:05:58 +0300

    add postID

--- src/pages/private/postId/PostId.jsx
+++ src/pages/private/postId/PostId.jsx
@@ -1,13 +1,79 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import logo from "../../../assets/img/logo.png";
+import ToMyPage from "../feed/ToMyPage";
+import { NavLink } from "react-router-dom";
+import AliceCarousel from "react-alice-carousel";
+import Feed from "../feed/Feed";
+// eslint-disable-next-line no-unused-vars
+import PostStyle from "./postid.scss";
 
 export default function PostId() {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
 
-    let { id } = useParams();
+  useEffect(() => {
+    // fetch post data using the ID
+    fetch(`http://65.109.13.139:9191/post/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setPost(data));
+  }, [id]);
 
-    return(
-        <>
-            <p>Post:{id}</p>
-        </>
-    );
+  if (!post) {
+    return <div className="loader">Loading...</div>;
+  }
+
+  return (
+    <div className="main__postid">
+      <header className="postid__header">
+        <NavLink to="/">
+          <figure className="header__logo">
+            <img src={logo} alt="logo" />
+          </figure>
+        </NavLink>
+
+        <NavLink to="/my-page">
+          <div className="header__me">
+            <ToMyPage />
+          </div>
+        </NavLink>
+      </header>
+
+      <article className="blog-post">
+        <div className="blog-post__info">
+          <h2 className="blog-post__title">{post.title}</h2>
+          <p>{post.description}</p>
+          <span className="posts__container">
+            {post.image && post.video ? (
+              <AliceCarousel className="blog-post__img">
+                <img src={post.image} alt={post.image} />
+                <video autoPlay loop muted className="container__video">
+                  <source src={post.video} />
+                </video>
+              </AliceCarousel>
+            ) : (
+              <img src={post.image} alt="" className="container__img" /> || (
+                <video autoPlay loop muted className="container__video">
+                  <source src={post.video} />
+                </video>
+              )
+            )}
+          </span>
+
+          <a href="#" className="blog-post__cta">
+            Follow me
+          </a>
+        </div>
+      </article>
+      <NavLink to="/feed">
+        <div className="blog-post__cta">Favoriets from students</div>
+      </NavLink>
+    </div>
+  );
 }
