34dc1a91a1f14953616707aba94ae16e891a1343
Alina Muzicuka <alina.muzicuka@gmail.com>
2023-04-26 15:23:27 +0200

    likes

--- src/pages/private/feed/PostCard.jsx
+++ src/pages/private/feed/PostCard.jsx
@@ -1,13 +1,13 @@
 import * as React         from 'react';
-import { Avatar, Typography, Button, CardHeader, CardMedia, CardContent, CardActions, Card  } from '@mui/material';
+import { Typography, Button, CardHeader, CardMedia, CardContent, CardActions, Card  } from '@mui/material';
 import AliceCarousel      from "react-alice-carousel";
 import                          "react-alice-carousel/lib/alice-carousel.css";
 import { useNavigate }    from 'react-router-dom';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import Avatar from '@mui/joy/Avatar';
 import AvatarGroup from '@mui/joy/AvatarGroup';
 import { useStores } from '../../../stores/MainStore';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 
 export default function PostCard(props) {
     const { RequestsStore, ConfigStore } = useStores();
@@ -15,9 +15,7 @@ export default function PostCard(props) {
     const [iconColor, setIconColor] = React.useState("action");
     const [avatars, setAvatars] = React.useState([]);
     const [surplus, setSurplus] = React.useState(0);
-    const [userLike, setUserLike] = React.useState([]);
 
-    const { id } = useParams();
 
     let item = props.item;
     const navigate = useNavigate();
@@ -28,41 +26,30 @@ export default function PostCard(props) {
         })
           .then((data) => {
             setLikes(data.likes);
-            setIconColor("red");
+            setIconColor("error");
          })
           .catch((error) => console.error(error));
       };
 
       useEffect(() => {
-        if (item) {
-            RequestsStore.doGet(ConfigStore.url + "/likes/" + item._id)
-            .then((res) => {
-                if(res.count >= 0) {
-                    return res.data.map((item) => (item.fromUser.username));
-                }
-            })
-            .then((data) => {
-                setUserLike(data);
-                if(data.includes(ConfigStore.me.username)) {
-                    setIconColor("red");
-                }
-            })
+        if(item) {
+            item.likes.find((liked) => liked.fromUser) ? setIconColor("error") : setIconColor("action");
         }
-      }, [item]);
+      }, [item.likes]);
 
   return (
-        <Card sx={{ width: 600 }}>
+        <Card sx={{ width: 600 }} key={item._id}>
             <CardHeader 
             avatar={
-              <Avatar onClick = {() => navigate(`/user/${item.author.username}`)}
+              <Avatar onClick = {() => navigate(`/user/${item.user[0].username}`)}
                 className="user__avatar"
-                src={item && item.author ? item.author.avatar : 'some defaul src'}
+                src={item && item.user[0] ? item.user[0].avatar : 'some defaul src'}
                 alt="my avatar"
                 sx={{ width: 60, height: 60 }}
                 aria-label="recipe"
               ></Avatar>
             }
-            title={item && item.author ? item.author.username : 'some defaul src'}
+            title={item && item.user[0] ? item.user[0].username : 'some defaul src'}
           />
             {item.video && item.video !== "any" && item.image ? 
                 <AliceCarousel disableButtonsControls='true' touchTracking='true' touchMoveDefaultEvents='false'>
@@ -108,13 +95,12 @@ export default function PostCard(props) {
                 </Typography>
             </CardContent>
             <FavoriteIcon sx={{ ml: 2}} color={iconColor} onClick={handleLike} />
-            {/* <AvatarGroup>
-            {avatars.map((avatar, index) => (
-                <Avatar key={index} {...avatar} />
+            <AvatarGroup>
+            {item.likes.map((element, index) => (
+                <Avatar key={index + Math.random()} src={ConfigStore.url + "/avatar/" + element.fromUser}/>
                 ))}
-
                 {!!surplus && <Avatar>+{surplus}</Avatar>}
-            </AvatarGroup> */}
+            </AvatarGroup>
             <CardActions>
                 <Button size="small" onClick={() => navigate(`/post/${item._id}`)}>Learn More</Button>
             </CardActions>
